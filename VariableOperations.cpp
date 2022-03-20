8f2b6ecdc2106fdb9d00be0e1f36a990c46de876
Preyansh Khanna <93733796+Preyansh06@users.noreply.github.com>
2022-03-20 23:02:36 +0530

    Create VariableOperations.cpp

---
+++ VariableOperations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int n=operations.size();
+        int x=0;
+        for(int i=0;i<n;i++)
+        {
+            if(operations[i]=="X++")
+            {
+                x++;
+            }
+            else if(operations[i]=="++X")
+            {
+                ++x;
+            }
+            else if(operations[i]=="++X")
+                 {
+                     x--;
+                 }
+            else
+                --x;
+        }
+        return x;
+    }
+};
