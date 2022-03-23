f7a4458ea2e350324c9d8eaa97cab08b388eb325
Preyansh Khanna <93733796+Preyansh06@users.noreply.github.com>
2022-03-23 23:32:57 +0530

    Update MajorityElement.cpp

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,4 +1,4 @@
-// solution 1 
+// solution 1 (using unordered map , o(n)- solution)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -15,3 +15,21 @@ public:
         
     }
 };
+// solution 2 (using map)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size(),i;
+        map<int,int> count;
+        for(i=0;i<n;i++)
+        {
+            count[nums[i]]++;
+            if(count[nums[i]]>n/2)
+                break;
+        }
+        
+        return nums[i];
+        
+    }
+};
+
