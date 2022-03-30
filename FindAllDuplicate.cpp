12054582bc23a1ed00aaea2a4a10cabbf1cdfccc
Preyansh Khanna <93733796+Preyansh06@users.noreply.github.com>
2022-03-30 23:29:09 +0530

    Update FindAllDuplicate.cpp

--- FindAllDuplicate.cpp
+++ FindAllDuplicate.cpp
@@ -23,3 +23,24 @@ public:
         return res;
     }
 };
+//2nd approach
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        int count;
+        vector<int> res;
+        unordered_map<int,int> c;
+       
+        for(int i=0;i<n;i++)
+        {
+            c[nums[i]]++;
+        }
+        for(auto x : c)
+        {
+            if(x.second==2)
+                res.push_back(x.first);
+        }
+        return res;
+    }
+};
