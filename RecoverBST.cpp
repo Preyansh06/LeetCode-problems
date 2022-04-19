8d0bfbd2acafbe4c2c438d143f9ab3b2483a97c5
Preyansh Khanna <93733796+Preyansh06@users.noreply.github.com>
2022-04-19 23:49:49 +0530

    Create RecoverBST.cpp

---
+++ RecoverBST.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* last;
+private:
+    void inorder(TreeNode* root)
+    {
+        if(!root)
+            return;
+        inorder(root->left);
+        if(prev!= NULL && (root->val < prev->val))
+        {
+            if(first== NULL)
+            {
+                first=prev;
+                last=root;
+            }
+            else
+                last=root;
+        }
+        prev=root;
+            inorder(root->right);
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        first = last = NULL;
+        prev= new TreeNode(INT_MIN);
+        inorder(root);
+        swap(first->val,last->val);
+         
+        
+    }
+};
