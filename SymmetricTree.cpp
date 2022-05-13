56d23916524fd61dfdd841525b10f1367f8ba89f
Preyansh Khanna <93733796+Preyansh06@users.noreply.github.com>
2022-05-13 23:29:08 +0530

    Update SymmetricTree.cpp

--- SymmetricTree.cpp
+++ SymmetricTree.cpp
@@ -10,3 +10,43 @@ public:
         return f(root->left, root->right);
     }
 };
+
+easy slotuion----
+    class Solution{
+    private:
+    bool isSymmetricfast(TreeNode* rootleft,TreeNode* rootright){
+        if(rootleft==NULL && rootright==NULL){
+            return true;
+        }
+         if(rootleft!=NULL && rootright==NULL){
+            return false;
+        }
+         if(rootleft==NULL && rootright!=NULL){
+            return false;
+        }
+        
+        if(rootleft->val!=rootright->val){
+            return false;
+        }
+       
+       bool cnd1 = isSymmetricfast(rootleft->left,rootright->right);
+       bool cnd2 = isSymmetricfast(rootleft->right,rootright->left);
+       
+       if(cnd1==true && cnd2==true){
+           return true;
+       }
+       else{
+           return false;
+       }
+    }
+    
+    public:
+    // return true/false denoting whether the tree is Symmetric or not
+    bool isSymmetric(TreeNode* root)
+    {
+	    if(root==NULL){
+	        return true;
+	    }
+	    return isSymmetricfast(root->left,root->right);
+    }
+};
